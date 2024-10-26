5b4c463a30699679b1b875841917b1a528ed7d4b
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 16:38:59 +0200

    Learning use of random.nextInt

--- JavaRandomNumbers/src/Main.java
+++ JavaRandomNumbers/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Random; //importiamo la classe random
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//creiamo una nuova classe random
+		Random caso = new Random();
+		int x = caso.nextInt();//crea un numero casuale tra -2miliardi e +2miliardi
+		System.out.println(x);
+		//se vogliamo limitare il numero bisogna inserire il valore tra le parentesi
+		//ricordiamoci che i numeri partono da 0, quindi se si vuole
+		//partire da 1 bisogna aggiungere +1 fuori dalle parentesi
+		int y= caso.nextInt(6)+1;
+		System.out.println(y);
 	}
 
 }
